bb40852ef76c4837a7b9445df52754a69f220000
mxydl2009 <mxydl2009@163.com>
2021-06-25 14:47:32 +0800

    给SubMenu添加交互

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -4,7 +4,7 @@ import { MenuContext } from './Menu'
 import { MenuItemProps } from './MenuItem'
 
 export interface SubMenuProps {
-  index?: number;
+  index?: string;
   className?: string;
   title: string;
 }
@@ -18,16 +18,37 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, className, children }) 
   const subMenuClasses = classNames('submenu', {
     'menu-opened': opened
   })
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-    setOpen(!opened)
+  // SubMenu交互：当Menu横向展示时，hover显示下拉；当纵向时，点击显示下拉
+
+  // 使用计时器的方式来避免子组件不断触发MouseEnter和MouseLeave造成的闪烁问题
+  let timer: any;
+  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      e.preventDefault()
+      setOpen(toggle)
+    }, 500)    
   }
+  const handleVertical = context.mode === 'vertical' ? {
+    onClick: (e: React.MouseEvent) => {
+      e.preventDefault()
+      setOpen(!opened)
+    }
+  }: {}
+  const handleHorizontal = context.mode === 'horizontal' ? {
+    onMouseEnter: (e: React.MouseEvent) => {
+      handleMouse(e, true)
+    },
+    onMouseLeave: (e: React.MouseEvent) => {
+      handleMouse(e, false)
+    },
+  } : {}
   const renderChildren = () => {
     const childrenComponent = React.Children.map(children, (child, i) => {
       const childElement = child as FunctionComponentElement<MenuItemProps>
       const displayName = childElement.type.displayName
       if (displayName === 'MenuItem') {
-        return React.cloneElement(childElement, { index: i })
+        return React.cloneElement(childElement, { index: `${index}-${i}` })
       } else {
         console.error('Warning: SubMenu has a child which is not a MenuItem Component')
       }
@@ -42,8 +63,9 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, className, children }) 
     <li
       key={index}
       className={classes}
+      {...handleHorizontal}
     >
-      <div className={'submenu-title'} onClick={(e) => { handleClick(e) }}>
+      <div className={'submenu-title'} { ...handleVertical }>
         {title}
       </div>
       { renderChildren() }
