1d575834a454fa87d8d660defc9684225c3ecced
mxydl2009 <mxydl2009@163.com>
2021-06-28 16:29:03 +0800

    添加JSDoc注释用于生成文档

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,24 +2,40 @@ import React from 'react'
 import classNames from 'classnames'
 
 // 使用枚举类型来定义Button的size和type有哪些属性值，因为这些属性值都是一些固定的常量
-export enum ButtonSize {
-  Large = 'lg',
-  Small = 'sm'
-}
+// export enum ButtonSize {
+//   Large = 'lg',
+//   Small = 'sm'
+// }
 
-export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Danger = 'danger',
-  Link = 'link'
-}
+export type ButtonSize = "lg" | "sm"
+
+// export enum ButtonType {
+//   Primary = 'primary',
+//   Default = 'default',
+//   Danger = 'danger',
+//   Link = 'link'
+// }
+
+export type ButtonType = 'primary'|'default'|'danger'|'link'
 
 // 创建Button组件props的形状
 interface BaseButtonProps {
   className?: string;
+  /**
+   * 设置Button组件是否禁用
+   */
   disabled?: boolean;
+  /**
+   * 设置Button组件的尺寸
+   */
   size?: ButtonSize;
+  /**
+   * 设置Button组件的类型
+   */
   btnType?: ButtonType;
+  /**
+   * 当类型为link时，设置href属性
+   */
   href?: string;
   children: React.ReactNode;
 }
@@ -32,6 +48,18 @@ type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElemen
 // Utility Types: Partial<T>将T上所有的属性变为可选的，因为NativeButtonProps的必须属性不属于AnchorButtonProps的属性，所以直接将所有属性变为可选的；
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
+/**
+ * ## Button组件的说明
+ * ### 引入说明
+ * ~~~js
+ * // ESModule方式
+ * import { Button } from 'mxyrc'
+ * // CommonJS
+ * const { Button } = require('mxyrc')
+ * ~~~
+ * ### 简介
+ * 按钮元素，适用于完成点击等特定的交互行为
+ */
 const Button: React.FC<ButtonProps> = (props) => {
   const { btnType, className, disabled, size, href, children, ...restProps } = props;
   // 使用classNames来添加className
@@ -41,9 +69,9 @@ const Button: React.FC<ButtonProps> = (props) => {
     // 如果btnType存在，则添加了btn-${btnType}
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    disabled: btnType === ButtonType.Link ? disabled: false
+    disabled: btnType === 'link' ? disabled: false
   });
-  if (btnType === ButtonType.Link && href) {
+  if (btnType === 'link' && href) {
     // 如果是link类型且传入了href属性，则返回a元素
     return (
       <a
@@ -69,7 +97,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: ButtonType.Default
+  btnType: 'default'
 }
 
 export default Button
