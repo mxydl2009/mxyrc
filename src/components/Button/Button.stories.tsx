9169501f5bcfa137974b94ee27b81c8f1dcf4793
mxydl2009 <mxydl2009@163.com>
2021-06-28 16:10:56 +0800

    使用CSF模式重写stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { Meta, Story } from '@storybook/react';
 import { action } from '@storybook/addon-actions'
-import {} from '@storybook/addon-docs'
 
 import Button from './Button';
 
-// 使用传统的Stories API来写stories
+export default {
+  title: "Button组件",
+  component: Button
+} as Meta
 
-// 定义story case
-const DefaultButton = () => {
+export const DefaultButton: Story = () => {
   return <Button
     onClick={action('dianji')}
   >
@@ -16,7 +17,9 @@ const DefaultButton = () => {
   </Button>
 }
 
-const ButtonWithSize = () => {
+DefaultButton.storyName = '默认的Button'
+
+export const ButtonWithSize: Story = () => {
   return (
     <>
       <Button size="lg">Large</Button>
@@ -25,7 +28,9 @@ const ButtonWithSize = () => {
   )
 }
 
-const ButtonWithType = () => {
+ButtonWithSize.storyName = '不同尺寸的Button'
+
+export const ButtonWithType: Story = () => {
   return (
     <>
       <Button btnType="primary">primary</Button>
@@ -34,8 +39,42 @@ const ButtonWithType = () => {
     </>
   )
 }
+
+ButtonWithType.storyName = '不同类型的Button'
+
+
+// 使用传统的Stories API来写stories
+
+// 定义story case
+// const DefaultButton = () => {
+//   return <Button
+//     onClick={action('dianji')}
+//   >
+//     默认Button
+//   </Button>
+// }
+
+// const ButtonWithSize = () => {
+//   return (
+//     <>
+//       <Button size="lg">Large</Button>
+//       <Button size="sm">Small</Button>
+//     </>
+//   )
+// }
+
+// const ButtonWithType = () => {
+//   return (
+//     <>
+//       <Button btnType="primary">primary</Button>
+//       <Button btnType="danger">danger</Button>
+//       <Button btnType="link">link</Button>
+//     </>
+//   )
+// }
+
 // storiesOf定义store的名称，使用add()添加每一个stories，使用这种方式可以定义中文的story case 名称，比CSF方式更加方便一些；
-storiesOf('Button Component', module)
-  .add('默认的Button', DefaultButton)
-  .add('不同尺寸的Button', ButtonWithSize)
-  .add('不同类型的Button', ButtonWithType)
+// storiesOf('Button Component', module)
+//   .add('默认的Button', DefaultButton)
+//   .add('不同尺寸的Button', ButtonWithSize)
+//   .add('不同类型的Button', ButtonWithType)
