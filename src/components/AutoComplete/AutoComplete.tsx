5314888e206b2ba948effa0fd91e51108c4775c5
mxydl2009 <mxydl2009@163.com>
2021-07-05 16:35:28 +0800

    AutoComplete组件, 添加支持自定义渲染菜单项的方法

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -3,23 +3,40 @@ import classNames from 'classnames'
 import Input, { InputProps } from '../Input/Input'
 
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => string[];
-  onSelect?: (item: string) => void
+  /**
+   * 筛选函数，根据输入的数据进行筛选，返回筛选后的数据
+   */
+  fetchSuggestions: (data: string) => DataSourceType[];
+  /**
+   * 选择某一项后的回调函数
+   */
+  onSelect?: (item: DataSourceType) => void;
+  /**
+   * 自定义如何渲染待选的菜单项
+   */
+  renderOption?: (item: DataSourceType) => React.ReactElement;
 }
+
+interface DataSourceObj {
+  value: string;
+}
+
+// DataSourceType必须是包含value属性的对象，更多的属性由泛型T指定
+export type DataSourceType<T = {}> = T & DataSourceObj
 
 const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const {
     fetchSuggestions,
     onSelect,
     value,
+    renderOption,
     ...restProps
   } = props
   const [inputValue, setInputValue] = useState(value)
-  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
   
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
-    console.log()
     setInputValue(value)
     if (value) {
       const result = fetchSuggestions(value)
@@ -28,20 +45,20 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
       setSuggestions([])
     }
   }
-  const handleSelect = (item: string) => {
-    setInputValue(item)
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value)
     setSuggestions([])
     onSelect && onSelect(item)
   }
   const generateDropdown = () => {
     return (
       <ul>
-        { suggestions.map((item: string, index: number) => 
+        { suggestions.map((item: DataSourceType, index: number) => 
           <li 
             key={index}
             onClick={() => { handleSelect(item)}}
           >
-            {item}
+            { renderOption? renderOption(item): item.value }
           </li> )}
       </ul>
     )
