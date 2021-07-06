4f07c928f7fcf1aaf5cce55ffc01fb7d72c4c21f
mxydl2009 <mxydl2009@163.com>
2021-07-06 15:42:50 +0800

    AutoComplete组件添加支持键盘事件

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,8 +1,10 @@
-import React, { FC, useState, ChangeEvent, useEffect } from 'react'
+import React, { FC, useState, ChangeEvent, useEffect, useRef, KeyboardEvent } from 'react'
 import classNames from 'classnames'
 import Input, { InputProps } from '../Input/Input'
 import Icon from '../Icon/Icon'
+import Transition from '../Transition/Transition'
 import useDebounce from '../../hooks/useDebounce'
+import useClickOutside from '../../hooks/useClickOutside'
 
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
   /**
@@ -37,9 +39,17 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const [inputValue, setInputValue] = useState(value)
   const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
   const [loading, setLoading] = useState(false)
+  const [highlightIndex, setHighlightIndex] = useState(-1)
+  const triggerSearch = useRef(true)
+  const componentRef = useRef<HTMLDivElement>(null)
   const debouncedValue = useDebounce(300, inputValue)
+
+  useClickOutside(componentRef, () => {
+    setSuggestions([])
+  })
+
   useEffect(() => {
-    if (debouncedValue) {
+    if (debouncedValue && triggerSearch.current) {
       const result = fetchSuggestions(debouncedValue)
       if (result instanceof Promise) {
         setLoading(true)
@@ -53,35 +63,80 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
     } else {
       setSuggestions([])
     }
+    setHighlightIndex(-1)
   }, [debouncedValue])
   
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
+    triggerSearch.current = true
   }
   const handleSelect = (item: DataSourceType) => {
     setInputValue(item.value)
     setSuggestions([])
     onSelect && onSelect(item)
+    triggerSearch.current = false
   }
+
+  const highlight = (index: number) => {
+    if (index < 0) {
+      index = 0
+    }
+    if (index >= suggestions.length) {
+      index = suggestions.length - 1
+    }
+    setHighlightIndex(index)
+  }
+
+  const handleKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch(e.keyCode) {
+      case 13:
+        if (suggestions[highlightIndex]) {
+          handleSelect(suggestions[highlightIndex])
+        }
+        break;
+      case 38:
+        highlight(highlightIndex - 1)
+        break;
+      case 40:
+        highlight(highlightIndex + 1)
+        break;
+      case 27:
+        setSuggestions([])
+        break;
+      default:
+        break;
+    }
+  }
+  
   const generateDropdown = () => {
     return (
-      <ul>
-        { suggestions.map((item: DataSourceType, index: number) => 
-          <li 
-            key={index}
-            onClick={() => { handleSelect(item)}}
-          >
-            { renderOption? renderOption(item): item.value }
-          </li> )}
+      <ul className="mxyrc-suggestion-list">
+        { 
+          suggestions.map((item: DataSourceType, index: number) => {
+            const cNames = classNames('suggestion-item', {
+              'item-highlighted': index === highlightIndex
+            })
+            return (
+              <li 
+                key={index}
+                className={cNames}
+                onClick={() => { handleSelect(item)}}
+              >
+                { renderOption? renderOption(item): item.value }
+              </li>
+            ) 
+          })
+        }
       </ul>
     )
   }
   return (
-    <div className="mxyrc-auto-complete">
+    <div className="mxyrc-auto-complete" ref={componentRef}>
       <Input
         value={inputValue}
         onChange={handleChange}
+        onKeyDown={handleKeydown}
         {...restProps}
       />
       { loading && <ul><Icon icon={'spinner'} spin /></ul> }
